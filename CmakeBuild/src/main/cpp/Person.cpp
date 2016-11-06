fe13bb8daa673b9c3bb99b92a83d45bee2bf8ee6
Charles <oksisi213@gmail.com>
2016-11-07 00:52:48 +0900

    수정

--- CmakeBuild/src/main/cpp/Person.cpp
+++
@@ -1,9 +0,0 @@
-//
-// Created by Charles on 11/4/16.
-//
-
-#include "Person.h"
-
-char *Person::say() {
-    return msg;
-}
--- CmakeBuild/src/main/java/com/maxst/www/ndkpractice/MainActivity.java
+++ CmakeBuild/src/main/java/com/maxst/www/ndkpractice/MainActivity.java
@@ -11,7 +11,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         TextView tv = (TextView) findViewById(R.id.tv);
-        tv.setText(NativeManager.getInstance().getSomething2());
+        tv.setText(NativeManager.getInstance().getSomething());
     }
 
 
